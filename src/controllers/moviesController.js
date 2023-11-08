84932b20ad17799d63eec239f88e9b92e18a7c8f
trinidad <daianatrinidadpuig@gmail.com>
2023-11-08 15:48:59 -0300

    movies creado

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,25 @@
+const movies = require("../database/models/movies");
+const movieService= require("../services/movie-service");
+
+module.exports={
+    list:(req,res)=>{
+        movieService.getAllMovies().then((movies)=>{
+        res.render("moviesList",{movies});
+        });
+    },
+    new:(req,res)=>{
+        movieService.getNewestMovies().then((movies)=>{
+            res.render("newestMovies",{movies});
+        });
+    },
+    recomended:(req,res)=>{
+        movieService.getRecomendedMovies().then((movies)=>{
+        res.render("recommendedMovies",{movies});
+        });
+    },
+    detail:(req,res)=>{
+        movieService.getMovieDetail(req.params.id).then((movie)=>{
+        res.render("moviesDetail",{movie});
+        })
+    }
+};
